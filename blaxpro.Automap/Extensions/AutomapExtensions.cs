e7aeb76915ae8a8485c40150b2bcdce5052f840e
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2019-08-07 13:49:14 +0200

    add not implemented AutomapExtensions class

---
+++ blaxpro.Automap/Extensions/AutomapExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace blaxpro.Automap.Extensions
+{
+    public static class AutomapExtensions
+    {
+        public static T mapTo<T>(this object item) where T: class, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static T mapTo<T>(this object item, T targetItem) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public static T mapToOrDefault<T>(this object item) where T : class, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static T mapToOrDefault<T>(this object item, T targetItem) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
