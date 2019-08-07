41ca4c1ee10aa96654270d2f8985e6407bc44d04
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2019-08-07 13:49:25 +0200

    add some tests

--- blaxpro.Automap.Tests/UnitTest1.cs
+++ blaxpro.Automap.Tests/UnitTest1.cs
@@ -1,14 +1,70 @@
-using System;
+using blaxpro.Automap.Extensions;
 using Xunit;
 
 namespace blaxpro.Automap.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        public class First
         {
+            public int IntProperty { get; set; }
+            public string StringProperty { get; set; }
+        }
 
+        public class Second
+        {
+            public int Intproperty { get; set; }
+            public string StringProperty { get; set; }
+            public string Stringproperty { get; set; }
+            public decimal? DoubleNullableProperty { get; set; }
+        }
+
+        [Fact(DisplayName = "Map to new instance")]
+        public void map_to_new_instance()
+        {
+            First first;
+            Second second;
+
+            first = new First
+            {
+                IntProperty = 34,
+                StringProperty = "Some string",
+            };
+
+            second = first.mapTo<Second>();
+
+            Assert.Equal(34, second.Intproperty);
+            Assert.Equal("Some string", second.StringProperty);
+            Assert.Null(second.Stringproperty);
+            Assert.Null(second.DoubleNullableProperty);
+        }
+
+        [Fact(DisplayName = "Map to existing instance")]
+        public void map_to_existing_instance()
+        {
+            First first;
+            Second second;
+
+            first = new First
+            {
+                IntProperty = 34,
+                StringProperty = "Some string",
+            };
+
+            second = new Second
+            {
+                Intproperty = 666,
+                StringProperty = "aaa",
+                Stringproperty = "untouched",
+                DoubleNullableProperty = 3.1415m
+            };
+
+            first.mapTo(second);
+
+            Assert.Equal(34, second.Intproperty);
+            Assert.Equal("Some string", second.StringProperty);
+            Assert.Equal("untouched", second.Stringproperty);
+            Assert.Equal(3.1415m, second.DoubleNullableProperty);
         }
     }
 }
