56bf75ab7b5935728a87de2d0f25ff0ccdd81ae8
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2019-08-14 09:48:00 +0200

    add IMapRepository and its default implementation

---
+++ blaxpro.Automap/Services/IMapRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using blaxpro.Automap.Models;
+
+namespace blaxpro.Automap.Services
+{
+    public interface IMapRepository
+    {
+        IMap getMap(Type sourceType, Type targetType);
+    }
+}
