e56126043b7d839016372a0db8abfedc135ffba5
hossmi <hossmi@gmail.com>
2019-11-11 22:00:09 +0100

    add LooseMapBuilder for relaxed property copy

---
+++ Blacksmith.Automap/Services/LooseMapBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Blacksmith.Automap.Services
+{
+    public class LooseMapBuilder : AbstractMapBuilder
+    {
+        protected override void processUnpairedSourceProperty(Type sourceType, Type targetType, PropertyInfo property)
+        {
+        }
+
+        protected override void processUnpairedTargetProperties(Type sourceType, Type targetType, IEnumerable<PropertyInfo> targetProperties)
+        {
+        }
+    }
+}
